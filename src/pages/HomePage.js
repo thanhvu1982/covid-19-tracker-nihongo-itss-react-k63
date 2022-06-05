b96afcc874648c5d40a6c072067722c912d8de06
Phung Dinh Son <62043279+phungson185@users.noreply.github.com>
2022-06-05 15:10:02 +0700

    fix: refactor use data (#3)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,45 +3,26 @@ import Home from '../components/Home';
 import { useEffect, useState } from 'react';
 import { homeService } from '../services';
 import NewEpidemic from '../components/NewEpidemic';
-
+import Loading from '../common/Loading';
 const HomePage = () => {
-  const [data, setDatas] = useState({});
+  const [caseInfo, setCaseInfo] = useState({});
 
   useEffect(() => {
-    homeService.fetchCaseInfo.then((caseInfo) => {
-      const today = caseInfo.data[caseInfo.data.length - 1];
-      const yesterday = caseInfo.data[caseInfo.data.length - 2];
-      const isToday =
-        parseInt(caseInfo.meta.lastUpdated.split(', ')[1].split('/'), 10) ===
-        new Date().getDate();
-      setDatas({
-        isToday,
-        confirmed: isToday ? today.newConfirmed : yesterday.newConfirmed,
-        recovered: isToday ? today.newRecovered : yesterday.newRecovered,
-        deaths: isToday ? today.newDeaths : yesterday.newDeaths,
-        curing: isToday ? today.newCuring : yesterday.newCuring,
-        totalConfirmed: today.totalConfirmed2020,
-        totalRecovered: today.totalRecovered2020,
-        totalDeaths: today.totalDeaths2020,
-        totalCuring: today.totalCuring,
-        lastUpdated: caseInfo.meta.lastUpdated,
-        from274Confirmed: today.totalConfirmed,
-        from274Deaths: today.totalDeaths,
-      });
-    });
+    homeService.fetchCaseInfo.then((data) => setCaseInfo(data));
   }, []);
 
   return (
+    caseInfo.data ? 
     <Box sx={{ width: '100%', py: 4 }}>
       <Container>
         <Box>
-          <Home data={data} />
+          <Home caseInfo={caseInfo} />
         </Box>
         <Box mt={4}>
-          <NewEpidemic data={data} />
+          <NewEpidemic caseInfo={caseInfo} />
         </Box>
       </Container>
-    </Box>
+    </Box> : <Loading />
   );
 };
 
