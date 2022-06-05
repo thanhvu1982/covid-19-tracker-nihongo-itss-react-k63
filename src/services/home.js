3494add39acd2c980a24c84fb5b0a07c4796339b
Tran Bao Ngoc <54113717+ngoctb0102@users.noreply.github.com>
2022-06-05 23:39:09 +0700

    feat: add province by day (#7)
    Co-authored-by: lolislayer <>

--- src/services/home.js
+++ src/services/home.js
@@ -2,9 +2,12 @@ import { config } from '../configs/config';
 import { client } from './axios';
 
 const fetchCaseInfo = client.get(config.caseByDay);
-const fetchCaseByProvinces = client.get(config.caseByProvince)
+const fetchCaseByProvinces = client.get(config.caseByProvince);
+const fetchCaseByDayOfProvinces = client.get(config.caseByDayOfProvinces);
+
 
 export default {
   fetchCaseInfo,
   fetchCaseByProvinces,
+  fetchCaseByDayOfProvinces
 };
