d65607cfe023623025f50bc1f73910bc4530d55e
phungson185 <phungson185@gmail.com>
2022-06-05 23:21:47 +0700

    feat: add province by day charts

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,14 +6,16 @@ import NewEpidemic from '../components/NewEpidemic';
 import Loading from '../common/Loading';
 import VietnamCasesByDayChart from '../components/VietNamCasesByDayChart';
 import ProvinceCharts from '../components/ProvinceCharts';
-
+import ProvinceByDayChart from '../components/ProvinceByDayCharts';
 const HomePage = () => {
   const [caseInfo, setCaseInfo] = useState({});
   const [caseByProvinces, setCaseByProvinces] = useState({});
+  const [caseByDayOfProvinces, setCaseByDayOfProvinces] = useState({});
 
   useEffect(() => {
     homeService.fetchCaseInfo.then((data) => setCaseInfo(data));
     homeService.fetchCaseByProvinces.then((data) => setCaseByProvinces(data));
+    homeService.fethcCaseByDayOfProvinces.then((data) => setCaseByDayOfProvinces(data));
   }, []);
 
   return (
@@ -31,6 +33,9 @@ const HomePage = () => {
         </Box>
         <Box mt={4}>
           <ProvinceCharts caseByProvinces={caseByProvinces} />
+        </Box>
+        <Box mt={4}>
+          <ProvinceByDayChart caseByDayOfProvinces={caseByDayOfProvinces} />
         </Box>
       </Container>
     </Box> : <Loading />
--- src/services/home.js
+++ src/services/home.js
@@ -3,8 +3,10 @@ import { client } from './axios';
 
 const fetchCaseInfo = client.get(config.caseByDay);
 const fetchCaseByProvinces = client.get(config.caseByProvince)
+const fethcCaseByDayOfProvinces = client.get(config.caseByDayOfProvinces)
 
 export default {
   fetchCaseInfo,
   fetchCaseByProvinces,
+  fethcCaseByDayOfProvinces
 };
